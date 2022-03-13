91cbb1950faad96b0f53fd9ebdae4e33fca349c7
lzw <ziwei.ro@gmail.com>
2022-03-13 15:21:56 +0800

    google id

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -4,7 +4,7 @@ import json
 from datetime import datetime
 import os
 
-author: dict = scholarly.search_author_id('3WQTKocAAAAJ')
+author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
 scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
 name = author['name']
 author['updated'] = str(datetime.now())
